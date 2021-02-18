376455ea0e622f03649a08cca348c73e313a833f
nilayarat <79106843+nilayarat@users.noreply.github.com>
2021-02-18 15:38:54 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,21 @@
-#Explain your work
+DAY THREE HOMEWORK THAT INCLUDES STUDENTS NOTES 
 
-#Question 1
-for x in range(a):
-	print(a)
+list_students = { "Nilay":"Arat", "Ebru":"Yazar", "Can":"Oz", "Ezgi":"Orel", "Ay":"Savascisi" }
+info =  { "Nilay":"Arat", "Ebru":"Yazar", "Can":"Oz", "Ezgi":"Orel", "Ay":"Savascisi" }
+
+list_midterm = [50,60,70,80,90]
+list_final = [70,70,80,90,40]
+list_homework = [40,60,50,80,70]
+list_summary = (a+b+c)//3 
+
+print(list_summary) = [53,63,70,83,66]
+
+
+list_summary = int(input("Please enter a integer between 1 and 100: "))
+while list_summary <60 :
+	print("Sorry you are failed")
+while list_summary >= 60
+        print("Congrats!")
+	
+ 
+	
