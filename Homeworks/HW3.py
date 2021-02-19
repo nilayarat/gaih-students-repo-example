d70a6900cd2bae2e903136d76a07d2af235fd44c
nilayarat <79106843+nilayarat@users.noreply.github.com>
2021-02-19 22:56:08 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,48 @@
-#Explain your work
+#HANGMAN
+importing time module
+import.time
+#wait for 1 second
+time.sleep(1)
 
-#Question 1
-for x in range(a):
-	print(a)
+print "Guess!"
+time.sleep(0.5)
+
+#here we set the secret
+word = "flover"
+
+#creates an variable with an empty value
+guesses = ''
+
+#determine the number of turns
+turns = 7
+
+# Create a while loop
+#check if the turns are more than zero
+while turns > 0: 
+# make a counter that starts with zero
+    failed = 0             
+
+# for every character in flover_word    
+    for char in word:      
+
+# see if the character is in the players guess
+        if char in guesses:    
+    
+ # print then out the character
+            print char,    
+
+        else:
+# if not found, print a dash
+            print "_", 
+	
+# and increase the failed counter with one
+            failed += 1    
+
+# if failed is equal to zero
+
+# print You Won
+    if failed == 0:        
+        print "You won"  
+# exit the script
+	break
+       
