1208bf48591e944bc930851ce42736cb65db9bef
nilayarat <79106843+nilayarat@users.noreply.github.com>
2021-02-21 21:11:12 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,11 +1,33 @@
-#DAY 3 HOMEWORK THAT INCLUDES STUDENTS NOTES 
+#DAY 3 HOMEWORK THAT INCLUDES STUDENTS NOTES
 
-info =  { "Nilay":"Arat", "Ebru":"Yazar", "Can":"Oz", "Ezgi":"Orel", "Ay":"Savascisi" }
-
+list_name = ("Nilay", "Ebru", "Can", "Ezgi", "Ay")
+list_surname = ("Arat","Yazar", "Oz", "Orel", "Savascisi")
 list_midterm = [50,60,70,80,90]
 list_final = [70,70,80,90,40]
 list_homework = [40,60,50,80,70]
 print(list_summary) = [53,63,70,83,66]
+
+Info={"Nilay":53, "Ebru":63, "Can":70, "Ezgi":83, "Ay":66}
+for i in Info:
+got=Info[i]
+print("A",i,"got")
+{Nilay got 53}
+{Ebru got 63}
+{Can got 70}
+{Ezgi got 83}
+{Ay got 66}
+
+if i >=70
+print("Congrats") 
+ 
+else:
+print("Fail")
+
+Fail
+Fail
+Congrats
+Congrats
+Fail
 
 
 
