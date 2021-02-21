01c3148b01df27cbfb2e49ee1a7039b8198b5571
nilayarat <79106843+nilayarat@users.noreply.github.com>
2021-02-21 20:32:43 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -8,11 +8,7 @@ list_homework = [40,60,50,80,70]
 print(list_summary) = [53,63,70,83,66]
 
 
-list_summary = int(input("Please enter a integer between 1 and 100: "))
-while list_summary <60 :
-	print("Sorry you are failed")
-while list_summary >= 60
-        print("Congrats!")
+
 	
  
 	
